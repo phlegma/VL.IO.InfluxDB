dd1b49eee9e7162cb99d14f8c7f15f0558fc5cca
KA-SHK CO-Spare <dev@nilsweger.de>
2025-10-15 16:39:58 +0200

    query tests

---
+++ csharp/VL.IO.InfluxDBUtils/Utils.cs
@@ -0,0 +1,13 @@
+// For examples, see:
+// https://thegraybook.vvvv.org/reference/extending/writing-nodes.html#examples
+
+namespace VL.IO.InfluxDB;
+
+
+public static class Utils
+{
+    public static float DemoNode(float a, float b)
+    {
+        return a + b;
+    }
+}
